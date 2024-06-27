fde3518d97091eb46526531d31b2406ec498bac3
Ethan Wang <ethan_wang2@my.vcccd.edu>
2024-06-27 14:27:03 -0700

    First

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 def main():
     email = input('Enter your email: ')
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+
     result = True
+
+    if email[0].isalpha:
+        return True
+    elif len(email) <= 5 or len(email) >= 30:
+        return False
+    elif email.find('@'):
+        result = False
+    
+    else:
+        at_index = email.find('@')
+        if email.find('.', at_index) == 1:
+            result = False
+            
+    
+
 
     ########################################
     # Do not delete the return statement
     ########################################
     return result
-
 
 if __name__ == '__main__':
     main()
